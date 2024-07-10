87c9a2bb0072d29a83d76b6a5144e5dfab868c2f
Your Name <you@example.com>
2024-07-10 17:34:37 +0800

    1734

--- README.md
+++ README.md
@@ -6,11 +6,23 @@ AutoSite 是一个人工智能驱动的自动化网站机器人项目，旨在�
 
 1.安装python  
 2.克隆本项目到你的本地  
-3.在本项目中运行 pip install -r requirements.txt   
-4.在本项目中运行 python ai.py  
+3.使用命令行和ai交互运行 python ai.py  
+4.使用网页和ai交互需要运行先执行 pip install -r requirements.txt 然后再运行 python ai_web.py  
 5.如果一切正常，将会在浏览器自动打开一个网页，在网页中输入你的openai的apikey和apiurl点击确认后和ai对话  
 6.如果不正常的话，可以联系我解决：WoodChangeLY  
 7.没有apikey也可以联系我
+
+
+## 示例效果 
+
+需求描述：实现一个有趣的笑话网站，前端一个按钮，点击后请求后端，显示后端返回的笑话内容。
+
+### 对话
+![自动化部署示例](https://pub-20abb0d076b24b52a65a8f98d262b891.r2.dev/chat.png)
+
+### 效果
+![自动化部署示例](https://pub-20abb0d076b24b52a65a8f98d262b891.r2.dev/site.png)
+
 
 ## 推荐工具
 
--- ai.py
+++ ai.py
@@ -1,58 +1,34 @@
 from AI.agent.api import ModelApi
 from AI.agent.tools import RunTools
-from AI.agent.utils import load_history, save_history, open_browser
-from AI.agent.config import tools
+from AI.agent.config import prompt, tools
 
-from flask import Flask, request, jsonify, send_from_directory
-from flask_cors import CORS
 import os
 
-# 设置目录
-base_dir = os.path.join(os.getcwd(), 'AI')
-web_dir = os.path.join(base_dir, 'web')
-app = Flask(__name__, static_folder=web_dir, static_url_path='')
-CORS(app)
+# 从环境变量中获取OpenAI API密钥
+API_KEY = os.getenv('API_KEY')
+# 从环境变量中获取OpenAI API URL
+API_URL = os.getenv('API_URL')
 
-
-@app.route('/')
-def index():
-    return send_from_directory(app.static_folder, 'index.html')
-
-
-@app.route('/<path:path>')
-def serve_static(path):
-    return send_from_directory(app.static_folder, path)
-
-
-@app.route('/chat', methods=['POST'])
-def chat():
-    data = request.json
-    user_input = data['message']
-    conversation_id = data['conversationId']
-    api_key = data['apiKey']
-    api_url = data['apiUrl']
-
-    api = ModelApi(api_key, api_url)
-
-    messages = load_history(conversation_id)
-    messages.append({"role": "user", "content": user_input})
+def run():
+    messages = [{"role": "system", "content": prompt}]
+    api = ModelApi(API_KEY, API_URL)
 
     while True:
-        response = api.oneapi(messages, model="gpt-4o",
-                              tools=tools, choices="auto", response_format=None)
-        if response['type'] == 'message':
-            response_message = response['message']
-            messages.append({"role": "assistant", "content": response_message})
-            save_history(conversation_id, messages)
-            return jsonify({"message": response_message})
-        elif response['type'] == 'tools':
-            messages.append(response['message'])
-            tool_calls = response['tools']
-            call_messages_list = RunTools(tool_calls)
-            messages.extend(call_messages_list)
-            save_history(conversation_id, messages)
-
+        user_input = input("👤: ")
+        messages.append({"role": "user", "content": user_input})
+        while True:
+            response = api.oneapi(messages, model="gpt-4o", tools=tools, choices="auto", response_format=None)
+            if response['type'] == 'message':
+                response_message = response['message']
+                print("🤖:", response_message)
+                messages.append({"role": "assistant", "content": response_message})
+                break  
+            elif response['type'] == 'tools':
+                # print("工具响应调试:", response)
+                messages.append(response['message'])
+                tool_calls = response['tools']
+                call_messages_list = RunTools(tool_calls)
+                messages.extend(call_messages_list)
 
 if __name__ == "__main__":
-    open_browser()
-    app.run()
+    run()
